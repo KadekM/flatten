036934f9bd4409d6291ccea3f992ee1bb6607c1e
unknown <kadek.marek@gmail.com>
2015-01-30 18:35:54 -0800

    Part 09 finish

--- basics/src/main/scala/flatten/Part09.scala
+++ basics/src/main/scala/flatten/Part09.scala
@@ -9,4 +9,27 @@ trait Part09 {
   // Exercise: without looking at the previous part, create a type class `Serializable`, a function `toBytes` that impicitly uses this
   // typeclass, and instances for `Int` and `String`.
 
+  object Glue {
+    trait Serializable[A] {
+      def serialize(x: A): Array[Byte]
+    }
+
+    def toBytes[A](x:A)(implicit ser: Serializable[A]): Array[Byte] = ser.serialize(x)
+    def toBytes2[A : Serializable](x:A): Array[Byte] = implicitly[Serializable[A]].serialize(x)
+
+    implicit val IntSerialize = new Serializable[Int] {
+      def serialize(x: Int): Array[Byte] = x.toString.getBytes
+    }
+
+    implicit val StringSerialize = new Serializable[String] {
+      def serialize(x: String): Array[Byte] = x.getBytes
+    }
+
+    val myString = "Bananaphone!"
+    val myStringBytes = toBytes(myString)
+  }
+
+
+
+
 }
