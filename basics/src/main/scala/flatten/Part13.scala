bcf6944a228166f5d3b169318940b857f25d0d64
Marek Kadek <kadek.marek@gmail.com>
2015-01-30 18:56:03 -0800

    Part 13 and 14

--- basics/src/main/scala/flatten/Part13.scala
+++ basics/src/main/scala/flatten/Part13.scala
@@ -47,4 +47,11 @@ trait Part13 {
   // 2) Put all `Option[Future[A]]` into an Option Transformer, OptionT.
 
   // Exercise: Make a for-comprehension
+  val res = for {
+    username <- OptionT(Future.successful(getUserName(data)))
+    user <- OptionT(getUser(username))
+    email = getEmail(user)
+    validate <- OptionT(Future.successful(validateEmail(email)))
+    success <- OptionT(sendEmail(email))
+  } yield success
 }
--- basics/src/main/scala/flatten/Part14.scala
+++ basics/src/main/scala/flatten/Part14.scala
@@ -1,5 +1,7 @@
 package flatten
 
+import scala.concurrent.Future
+import scalaz.OptionT
 import scalaz.Scalaz._
 
 trait Part14 extends Part13 {
@@ -18,7 +20,13 @@ trait Part14 extends Part13 {
   val y2 = 5 |> double
 
   // Exercise, rewrite the for-comprehension from part 13, but use `|>` for applying Future.successful and EitherT.apply
-
+  for {
+    username <- getUserName(data) |> Future.successful |> OptionT.apply
+    user <- getUser(username) |> OptionT.apply
+    email = getEmail(user)
+    validatedEmail <- validateEmail(email) |> Future.successful |> OptionT.apply
+    success <- sendEmail(email) |> OptionT.apply
+  } yield success
 
   def double(i: Int) = i * 2
 }
